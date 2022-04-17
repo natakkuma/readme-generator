dac9e22ad8ef53ff81500782de9aaa0aa6716750
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-04-17 16:47:41 -0700

    update generate mark up and license section function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -77,14 +77,81 @@
     
   };
 
-  
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+//FUNCTION renderLicenseSection - function that returns the license section of README
+  function renderLicenseSection(license) {
+
+    //if Apache 2.0
+    if (license === 'Apache 2.0') {
+      return 'Please click the link above to view license.';
+    } 
+    
+    //else if GNU GPL v3
+    else if (license === 'GNU GPL v3') {
+      return 'Please click the link above to view license.';
+    } 
+    
+    //else if MIT
+    else if (license === 'MIT') {
+      return 'Please click the link above to view license.';
+    } 
+    
+    //else if ISC
+    else if (license === 'ISC') {
+      return 'Please click the link above to view license.';
+    } 
+    
+    //else if Mozilla 2.0
+    else if (license === 'Mozilla 2.0') {
+      return 'Please click the link above to view license.';
+    } 
+    
+    //else if Boost 1.0
+    else if (license === 'Boost 1.0') {
+      return 'Please click the link above to view license.';
+    } 
+    
+    //else if there is no license, return an empty string
+    else if (license === 'no license') {
+      return '';
+    }
+    
+  };
+
+//FUNCTION generateMarkdown - function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description 
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  ${data.installation}
+
+  ## Usage 
+  ${data.usage}
+
+  ## License 
+  This project is license under ${data.license}
+
+  ## Contributing 
+  ${data.contribution}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  For additional information or inquiries, please contact me directly at ${data.email}. Please feel free to view my other projects at https://github.com/${data.username}.
 
 `;
 }
